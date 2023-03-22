f1616369c335403daacdefc669dfa6272378ce06
arawat2002 <79430320+arawat2002@users.noreply.github.com>
2023-03-22 15:05:51 -0700

    Update heart_animation.ino

--- heart_animation.ino
+++ heart_animation.ino
@@ -1,106 +1,102 @@
-// Oscilloscope Heart Code
-//originally from sine generator code: https://forum.arduino.cc/index.php?topic=361759.0
-//which was base on http://forum.arduino.cc/index.php?topic=8563.0
-
-
-// Heart figure based on the following parametric equations
-//x = 5 (sin(t)).^3,
-//y = 4 cos(t) − 1.3 cos(2t) − 0.6 cos(3t) − 0.2 cos(4t)
-
-//connect pin 9 to ~8k-10k Ohm + (series with)100nF ceramic cap -> GND, tap the sinewave signal from the point at between the resistor and cap.
-//connect pin 10 to ~8k Ohm + (series with)100nF ceramic cap -> GND, tap the sinewave signal from the point at between the resistor and cap.
-
-float wav1[3];//[0] frequency, [1] unscaled amplitude, [2] is final amplitude
-float wav2[3];//[0] frequency, [1] unscaled amplitude, [2] is final amplitude
-int average1; 
-int average2; 
-const int Pin1 = 9;   
-const int Pin2 = 10;
-float time; 
-float percentage; 
-float templitude1;
-float templitude2;
-float offset = 2.5; // default value 2.5 volt as operating range voltage is 0~5V
-float minOutputScale = 0.0;
-float maxOutputScale = 5.0;
-const int resolution = 1; //this determines the update speed. A lower number means a higher refresh rate.
-const float pi = 3.14159;
-
-// coefficient for beating effect
-float coefficient = 1;
-// coefficient increases when stretch == ture, decreases whe stretch == false
-bool stretch = true;
-int count_stretch = 0;
-
+// The setXPin function set up 4 pins for 4-bit X coordinate
 void setup() {
-  wav1[0] = 50; //frequency of the sine wave
-  wav1[1] = 2.5; // 0V - 2.5V amplitude (Max amplitude + offset) value must not exceed the "maxOutputScale"
-  wav2[0] = 50; //frequency of the sine wave
-  wav2[1] = 2.5; // 0V - 2.5V amplitude (Max amplitude + offset) value must not exceed the "maxOutputScale"
-  
-  TCCR1B = TCCR1B & 0b11111000 | 1;//set timer 1B (pin 9 and 10) to 31250Hz.
-  pinMode(Pin1, OUTPUT);
-  pinMode(Pin2, OUTPUT); 
-
-  Serial.begin(9600); // used for testing 
+  pinMode(0, OUTPUT); // LSB
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT); // MSB
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
-void loop() {
+void SetX(uint8_t value)
+{
+  digitalWrite(0, (value>>0)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(1, (value>>1)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(2, (value>>2)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(3, (value>>3)&0x01);  // turn the LED on (HIGH is the voltage level)
+}
 
-  time = micros()% 1000000;
-  percentage = time / 1000000;
-  //Serial.println("*");
-  //Serial.println(percentage);
+void SetY(uint8_t value)
+{
+  digitalWrite(4, (value>>0)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, (value>>1)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(6, (value>>2)&0x01);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(7, (value>>3)&0x01);  // turn the LED on (HIGH is the voltage level)
+}
 
+void SetXY(uint8_t Xvalue, uint8_t Yvalue) {
+  PORTD = (Xvalue<<4 | (Yvalue&0x0F));
+}
 
-  if(percentage == 0.0){
-    Serial.println("*");
-    Serial.println(count_stretch);
-    count_stretch += 1;
-    if (count_stretch % 100 == 0) {
-      if (stretch) {
-        coefficient += 0.01;
-      }
-      else {
-        coefficient -= 0.01;
-      }
-
-      if (coefficient == 3.0){ // start shrinking when coefficient is 3
-        stretch = false; 
-      }
-      else if (coefficient == 1.0){ // start stretching when coefficient is 1
-        stretch = true;
-      }
-    }
+void draw_tri() {
+  int i = 0;
+  for(i = 0; i <=10; i++){
+    SetXY(i,i);
+    delayMicroseconds(10);
   }
 
-  //Serial.println(coefficient);
-  
-  // Serial.print("!", time)
+  for(i = 10; i >= 0; i--){
+      SetXY(i, 10);
+      delayMicroseconds(10);
+  }
 
-  templitude1 = pow(sin(((percentage) * wav1[0]) * 2 * pi),3);
-  wav1[2] = (templitude1 * wav1[1]) + offset; //shift the origin of sinewave with offset.
-  
-  //wav1[2] = wav1[2] * coefficient; // apply coefficient to x
-
-  templitude2 = 0.8*cos(((percentage) * wav2[0]) * 2 * pi) - 0.26*cos(((percentage) * wav2[0]) * 2 * pi * 2) - 0.12*cos(((percentage) * wav2[0]) * 2 * pi * 3) - 0.04*cos(((percentage) * wav2[0]) * 2 * pi * 4);
-  wav2[2] = (templitude2 * wav2[1]) + offset;
-  
-  //wav2[2] = wav2[2] * coefficient; // apply coefficient to y
-  
-  average1 = mapf(wav1[2],minOutputScale,maxOutputScale,0,255);
-  average2 = mapf(wav2[2],minOutputScale,maxOutputScale,0,255);
-  
-  analogWrite(9, average1);//set output "voltage"
-  analogWrite(10, average2);//set output "voltage"
-  delayMicroseconds(resolution);//this is to give the micro time to set the "voltage"
-  //
-  // wav1[0]-1
-  // wav2[0]-1
+  for(i = 10; i >= 0; i--){
+      SetXY(0, i);
+      delayMicroseconds(10);
+  }
 }
 
-// function to map float number with integer scale - courtesy of other developers.
-long mapf(float x, float in_min, float in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+void draw_square(){
+  int i = 0;
+  for(i = 0; i <=10; i++){
+    SetXY(i,0);
+    delayMicroseconds(10);
+  }
+
+  for(i = 0; i <=10; i++){
+    SetXY(10,i);
+    delayMicroseconds(10);  
+  }
+
+  for(i = 10; i >=0; i--){
+    SetXY(i,10);
+    delayMicroseconds(10);
+  }
+
+  for(i = 10; i >=0; i--){
+    SetXY(0,i);
+    delayMicroseconds(10);
+  }
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  
+  int count = 0;
+  while(count <= 1000){
+    draw_tri();
+    count += 1;
+  }
+
+  while(count >= 0){
+    draw_square();
+    count -= 1;
+  }
+/*
+  for(i = 0; i <=10; i++){
+    SetXY(i,0);
+    delayMicroseconds(100);
+  }
+
+  for(i = 10; i >=10; i--){
+    SetXY(i,1);
+    delayMicroseconds(100);
+  }
+*/
+  
+  
+
 }
