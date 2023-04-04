01378643126994212330d13fd30125e0bca2c6a4
Peggy <111605266+PaggieZ@users.noreply.github.com>
2023-04-04 15:32:40 -0700

    Update GLOBAL.h

--- GLOBAL.h
+++ GLOBAL.h
@@ -13,3 +13,8 @@ int drawdelay = 10;
 int K_x[] = {1, 1, 2, 2, 1, 1, 6, 6, 5, 5, 8, 7, 7, 13, 13, 12, 7, 12, 13, 13, 7, 7, 8, 5, 5, 6, 6, 1}; // X-Coordinate, the last index has the first point
 int K_y[] = {2, 3, 3, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 8, 3, 3, 2, 2, 3, 3, 6, 3, 3, 2, 2};
 int K_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, DOWN_RIGHT, RIGHT, DOWN, LEFT, UP, RIGHT, UP_LEFT, DOWN, RIGHT, DOWN, LEFT};
+
+int tri_x[] = {1, 4, 4, 1};
+int tri_y[] = {1, 1, 4, 1};
+int tri_direction[] = {RIGHT, UP, DOWN_LEFT};
+
