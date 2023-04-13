d74e657a7a9bba9e914986ab4b856ded669509fd
Peggy <111605266+PaggieZ@users.noreply.github.com>
2023-04-13 14:56:59 -0700

    Update GLOBAL.h

--- GLOBAL.h
+++ GLOBAL.h
@@ -20,45 +20,128 @@ rounded: 1, 1000
 int drawdelay = 10;
 int count = 250;
 
-uint8_t K_x[] = {1, 1, 2, 2, 1, 1, 6, 6, 5, 5, 8, 7, 7, 13, 13, 12, 7, 12, 13, 13, 7, 7, 8, 5, 5, 6, 6, 1}; // X-Coordinate, the last index has the first point
-uint8_t K_y[] = {2, 3, 3, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 8, 3, 3, 2, 2, 3, 3, 6, 3, 3, 2, 2};
-uint8_t K_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, DOWN_RIGHT, RIGHT, DOWN, LEFT, UP, RIGHT, UP_LEFT, DOWN, RIGHT, DOWN, LEFT};
-uint8_t K_size = 27;
+//A
 
+
+//B
+
+
+//C
+
+
+//D
+
+
+//E
 uint8_t E_x[] = {1, 1, 2, 2, 1, 1, 13, 13, 11, 11, 5, 5, 9, 9, 11, 11, 9, 9, 5, 5, 11, 11, 13, 13, 1};
 uint8_t E_y[] = {2, 3, 3, 13, 13, 14, 14, 11, 11, 12, 12, 9, 9, 10, 10, 6, 6, 7, 7, 4, 4, 5, 5, 2, 2};
 uint8_t E_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, UP, LEFT, DOWN, RIGHT, UP, RIGHT, DOWN, LEFT, UP, LEFT, DOWN, RIGHT, UP, RIGHT, DOWN, LEFT};
 uint8_t E_size = 24;
 
-uint8_t Y_x[] = {1, 1, 2, 7, 2, 1, 1, 7, 7, 6, 9, 12, 11, 11, 16, 16, 15, 5, 6, 6, 1};
-uint8_t Y_y[] = {2, 3, 3, 8, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 3, 3, 2, 2};
-uint8_t Y_direction[] = {UP, RIGHT, UP_RIGHT, UP_LEFT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_RIGHT, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, RIGHT, DOWN, LEFT};
-uint8_t Y_size = 20;
+//F
 
-uint8_t S_x[] = {2, 2, 5, 5, 9, 9, 2, 2, 12, 12, 10, 10, 5, 5, 12, 12, 2};
-uint8_t S_y[] = {2, 5, 5, 4, 4, 7, 7, 14, 14, 11, 11, 12, 12, 9, 9, 2, 2};
-uint8_t S_direction[] = {UP, RIGHT, DOWN, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, UP, LEFT, DOWN, RIGHT, DOWN, LEFT};
-uint8_t S_size = 16;
 
-uint8_t I_x[] = {5, 5, 7, 7, 5, 5, 12, 12, 10, 10, 12, 12, 5};
-uint8_t I_y[] = {2, 4, 4, 12, 12, 14, 14, 12, 12, 4, 4, 2, 2};
-uint8_t I_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, RIGHT, DOWN, LEFT};
-uint8_t I_size = 12;
-
+//G
 uint8_t G_x[] = {5, 3, 3, 5, 13, 13, 11, 11, 6, 5, 5, 6, 11, 11, 9, 9, 7, 7, 9, 9, 13, 13, 12, 5};
 uint8_t G_y[] = {2, 4, 12, 14, 14, 11, 11, 12, 12, 11, 5, 4, 4, 7, 7, 6, 6, 10, 10, 9, 9, 3, 2, 2};
 uint8_t G_direction[] = {UP_LEFT, UP, UP_RIGHT, RIGHT, DOWN, LEFT, UP, LEFT, DOWN_LEFT, DOWN, DOWN_RIGHT, RIGHT, UP, LEFT, DOWN, LEFT, UP, RIGHT, DOWN, RIGHT, DOWN, DOWN_LEFT, LEFT};
 uint8_t G_size = 23;
 
+//H
 uint8_t H_x[] = {3, 3, 4, 4, 3, 3, 7, 7, 6, 6, 10, 10, 9, 9, 13, 13, 12, 12, 13, 13, 9, 9, 10, 10, 6, 6, 7, 7, 3};
 uint8_t H_y[] = {3, 4, 4, 13, 13, 14, 14, 13, 13, 10, 10, 13, 13, 14, 14, 13, 13, 4, 4, 3, 3, 4, 4, 8, 8, 5, 4, 3, 3};
 uint8_t H_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, RIGHT, DOWN, LEFT, UP, RIGHT, UP, LEFT, DOWN, RIGHT, DOWN, LEFT};
 uint8_t H_size = 28;
 
+//I
+uint8_t I_x[] = {5, 5, 7, 7, 5, 5, 12, 12, 10, 10, 12, 12, 5};
+uint8_t I_y[] = {2, 4, 4, 12, 12, 14, 14, 12, 12, 4, 4, 2, 2};
+uint8_t I_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, RIGHT, DOWN, LEFT};
+uint8_t I_size = 12;
+
+//J
+
+
+//K
+uint8_t K_x[] = {1, 1, 2, 2, 1, 1, 6, 6, 5, 5, 8, 7, 7, 13, 13, 12, 7, 12, 13, 13, 7, 7, 8, 5, 5, 6, 6, 1}; // X-Coordinate, the last index has the first point
+uint8_t K_y[] = {2, 3, 3, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 8, 3, 3, 2, 2, 3, 3, 6, 3, 3, 2, 2};
+uint8_t K_direction[] = {UP, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, DOWN_RIGHT, RIGHT, DOWN, LEFT, UP, RIGHT, UP_LEFT, DOWN, RIGHT, DOWN, LEFT};
+uint8_t K_size = 27;
+
+//L
+
+
+//M
+
+
+//N
+
+
+//O
+
+
+//P
+
+
+//Q
+
+
+//R
+
+
+//S
+uint8_t S_x[] = {2, 2, 5, 5, 9, 9, 2, 2, 12, 12, 10, 10, 5, 5, 12, 12, 2};
+uint8_t S_y[] = {2, 5, 5, 4, 4, 7, 7, 14, 14, 11, 11, 12, 12, 9, 9, 2, 2};
+uint8_t S_direction[] = {UP, RIGHT, DOWN, RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, UP, LEFT, DOWN, RIGHT, DOWN, LEFT};
+uint8_t S_size = 16;
+
+//T
 uint8_t T_x[] = {7, 7, 3, 3, 13, 13, 9, 9, 7};
 uint8_t T_y[] = {2, 12, 12, 14, 14, 12, 12, 2, 2};
 uint8_t T_direction[] = {UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, LEFT};
 uint8_t T_size = 8;
+
+//U
+
+
+//V
+uint8_t V_x[] = {9, 3, 3, 2, 2, 6, 6, 5, 5, 9, 13, 13, 12, 12, 16, 16, 15, 15, 9};
+uint8_t V_y[] = {4, 10, 13, 13, 14, 14, 13, 13, 11, 7, 11, 13, 13, 14, 14, 13, 13, 10, 4};
+uint8_t V_direction[] = {UP_LEFT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, DOWN_RIGHT, UP_RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, DOWN_LEFT};
+uint8_t V_size = 18;
+
+//W
+uint8_t W_x[] = {5, 2, 2, 1, 1, 5, 5, 4, 4, 6, 8, 10, 12, 12, 11, 11, 15, 15, 14, 14, 11, 10, 8, 6, 5};
+uint8_t W_y[] = {5, 8, 13, 13, 14, 14, 13, 13, 9, 7, 9, 7, 9, 13, 13, 14, 14, 13, 13, 8, 5, 5, 7, 5, 5};
+uint8_t W_direction[] = {UP_LEFT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, DOWN_RIGHT, UP_RIGHT, DOWN_RIGHT, UP_RIGHT, UP, LEFT, UP, RIGHT, DOWN, LEFT, DOWN, DOWN_LEFT, LEFT, UP_LEFT, DOWN_LEFT, LEFT};
+uint8_t W_size = 24;
+
+//X
+uint8_t X_x[] = {1, 1, 2, 7, 3, 2, 2, 6, 6, 5, 8, 11, 10, 10, 14, 14, 13, 9, 14, 15, 15, 11, 11, 12, 8, 4, 5, 5, 1};
+uint8_t X_y[] = {3, 4, 4, 9, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 9, 4, 4, 3, 3, 4, 4, 8, 4, 4, 3, 3};
+uint8_t X_direction[] = {UP, RIGHT, UP_RIGHT, UP_LEFT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_RIGHT, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, DOWN_RIGHT, RIGHT, DOWN, LEFT, UP, RIGHT, UP_LEFT, DOWN_LEFT, RIGHT, DOWN, LEFT};
+uint8_t X_size = 28;
+
+//Y
+uint8_t Y_x[] = {1, 1, 2, 7, 2, 1, 1, 7, 7, 6, 9, 12, 11, 11, 16, 16, 15, 5, 6, 6, 1};
+uint8_t Y_y[] = {2, 3, 3, 8, 13, 13, 14, 14, 13, 13, 10, 13, 13, 14, 14, 13, 13, 3, 3, 2, 2};
+uint8_t Y_direction[] = {UP, RIGHT, UP_RIGHT, UP_LEFT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_RIGHT, UP_RIGHT, LEFT, UP, RIGHT, DOWN, LEFT, DOWN_LEFT, RIGHT, DOWN, LEFT};
+uint8_t Y_size = 20;
+
+//Z
+uint8_t Z_x[] = {3, 3, 11, 4, 4, 3, 3, 4, 4, 13, 13, 7, 13, 13, 14, 14, 13, 13, 3};
+uint8_t Z_y[] = {3, 4, 12, 12, 11, 11, 15, 15, 14, 14, 11, 5, 5, 6, 6, 2, 2, 3, 3};
+uint8_t Z_direction[] = {UP, UP_RIGHT, LEFT, DOWN, LEFT, UP, RIGHT, DOWN, RIGHT, DOWN, DOWN_LEFT, RIGHT, UP, RIGHT, DOWN, LEFT, UP, LEFT};
+uint8_t Z_size = 18;
+
+
+
+
+
+
+
+
+
 
 
 
